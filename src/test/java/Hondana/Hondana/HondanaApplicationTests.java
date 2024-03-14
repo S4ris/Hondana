1c77fcbe30e62df467dccedce9ba29d7086bdb98
Adam Wiszniewski <wiszniewskiadam95@gmail.com>
2024-03-14 11:35:59 +0100

    Initial commit

---
+++ src/test/java/Hondana/Hondana/HondanaApplicationTests.java
@@ -0,0 +1,13 @@
+package Hondana.Hondana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HondanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
